848c286efadeeb50fba22d5683852e3a17a548f9
Lisa <liza.sergeeva003@gmail.com>
2023-11-26 16:41:46 +0300

    feature2

--- Group/funcs.h
+++ Group/funcs.h
@@ -3,6 +3,8 @@
 
 double MySum(double, double);
 double MySub(double, double);
+double MySub(double, double);
+double MyMul(double, double);
 int count; // переменная для выбора в switch
 
 #endif // FUNCS
